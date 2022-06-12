005c6f4974179a5b0315a8e3dc09b8b7a86f9962
Pier-Luc Richard <pier-lucrichard@Pier-Lucs-MacBook-Air.local>
2022-06-12 14:33:22 -0400

    cd now changes the PWD and old PWD and i added new add_variable get_variable functions :D

--- src/builtin.c
+++ src/builtin.c
@@ -6,12 +6,84 @@
 /*   By: pirichar <pirichar@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:14:13 by pirichar          #+#    #+#             */
-/*   Updated: 2022/06/09 12:42:00 by pirichar         ###   ########.fr       */
+/*   Updated: 2022/06/12 14:32:48 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int	strarr_len(char **str_arr)
+{
+	int i;
+
+	i = 0;
+	while(str_arr[i])
+		i++;
+	return (i);
+}
+
+static void	add_new_variable(char **env,char *var, char *variable)
+{
+	int i;
+	char **tmp;
+	
+	tmp = env;
+	i = strarr_len(env);
+	env = malloc(sizeof(char *) * i + 2);
+	env[i + 2] = 0;
+	i = 0;
+	while(tmp[i])
+	{
+		env[i] = ft_strdup(tmp[i]);
+		i++;
+	}
+	env[i] = ft_strjoin(var, variable);
+	//free tmp probably here
+}
+
+void	set_variable(char **env, char *var, char *new_var)
+{
+	int i;
+	char *tmp;
+
+	i = 0;
+	while(env[i])
+	{
+		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0)
+		{
+			tmp = env[i];
+			env[i] = ft_strjoin(var, new_var);
+			free(tmp);
+			break;
+		}
+		i++;
+	}
+	add_new_variable(env, var, new_var);
+	//look for variable first
+	//if you find it replace it
+	//if you dont find it create it 
+		//create a new array 
+}
+
+void	mini_cd(char **s_line, char **new_env)
+{
+	/*change the OLDPWD= folder*/
+	char *actual_pwd;
+	char *buff;
+	
+	buff = NULL;
+	actual_pwd = getcwd(buff, 1024);
+	set_variable(new_env, "OLDPWD=", actual_pwd);
+	free(actual_pwd);
+	free(buff);
+	chdir(s_line[1]);
+	/*change the PWD*/
+	buff = NULL;
+	actual_pwd = getcwd(buff, 1024);
+	set_variable(new_env, "PWD=", actual_pwd);
+	free(actual_pwd);
+	free(buff);
+}
 //s'assurer que dans la version officielle echo "-n" 
 // fonctionne aussi mais pas echo " -n"
 void	mini_echo(char **s_line)
@@ -37,13 +109,6 @@ void	mini_echo(char **s_line)
 		printf("\n");
 }
 
-void	mini_cd(char **s_line)
-{
-	/*change the OLDPWD= folder*/
-	chdir(s_line[1]);
-	/*change the PWD*/
-
-}
 
 void	mini_pwd()
 {
@@ -81,7 +146,7 @@ void	look_for_builtins(char **s_line, char **new_env, bool *b_in)
 	else if(ft_strncmp(s_line[0], "cd",3) == 0)
 	{
 		*b_in = true;
-		mini_cd(s_line);
+		mini_cd(s_line, new_env);
 	}
 	else if(ft_strncmp(s_line[0], "export",8) == 0)
 	{
